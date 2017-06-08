0985b9cb64d8c4456b2a4dac223bb24c9805812a
Cody Chase <Cody Chase>
2017-06-08 18:54:33 -0400

    add on click for gifs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,5 +1,7 @@
 var topics = ["Finding Nemo","Finding Dory","Lilo and Stitch","Shrek"];
 var queryURL;
+var myStills=[], myAnimated=[];
+var isStill = [true,true,true,true,true,true,true,true,true,true,];
 
  
 btnCreate();
@@ -10,7 +12,7 @@ function btnCreate(){
     $("#nav").empty();
     for(var i = 0; i < topics.length; i++){
         var topicBtn = $("<button>");
-        queryURL = "http://api.giphy.com/v1/gifs/search?q="+topics[i]+"&api_key=dc6zaTOxFJmzC&limit=100&rating=pg&sort=relative";        
+        queryURL = "https://api.giphy.com/v1/gifs/search?q="+topics[i]+"&api_key=dc6zaTOxFJmzC&limit=100&rating=pg&sort=relative";        
         topicBtn.addClass("topic-button");
         topicBtn.attr("data-URL", queryURL);
         topicBtn.html(topics[i]);
@@ -36,8 +38,32 @@ $(".topic-button").on("click",function(){
                 method: "GET"
             }).done(function(response){
                 for(var j = 0; j < 10; j++){
-                $("#gifs").append("<div class='gifDiv'> <img src='"+response.data[j].images.fixed_height_still.url+"' class='gif'> <p>rating: "+response.data[j].rating+"</p>  </div>");
-                }
+                    $("#gifs").append("<div class='gifDiv'> <img src='"+response.data[j].images.original_still.url+"' data-index='" + j + "' class='gif'> <p>rating: "+response.data[j].rating+"</p>  </div>");
+                    myStills[j] = response.data[j].images.original_still.url; //this still url;
+                    myAnimated[j] = response.data[j].images.original.url; //this animated url; 
+                } 
+                    btnClick(); 
             });
 });
+}
+                      
+//on click function
+function btnClick(){
+    console.log("is running");
+    $(".gif").on("click",function(){
+        console.log("clicks");
+        var index = $(this).attr("data-index");
+        if (isStill[index]=== true){
+            //change to moving
+            $(this).attr("src", myAnimated[index]);
+            //update boolean
+            isStill[index] = false;
+        }else{
+            //change to still
+            $(this).attr("src", myStills[index]);
+            //update boolean
+            isStill[index] = true;
+        }
+    });
+    
 }
